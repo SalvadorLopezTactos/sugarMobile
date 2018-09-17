59a2da370d7b9a04548c8a7dcdf16b8cc2468a90
Salvador Lopez <salvador.lopez@tactos.com.mx>
2018-09-17 11:48:46 -0500

    Se bloquea campo Estado cuando la reunión ya ha sido Realizada o No Realizada

--- custom/views/meetings-edit-view/meeting-edit.js
+++ custom/views/meetings-edit-view/meeting-edit.js
@@ -6,6 +6,8 @@ const EditView = require('%app.views.edit%/modules/meetings-calls-edit-view.js')
 const MeetingEditView = customization.extend(EditView, {
     initialize(options) {
         this._super(options);
+
+        this.model.on('sync', this.readOnlyStatus,this);
         
     },
 
@@ -18,6 +20,25 @@ const MeetingEditView = customization.extend(EditView, {
             this.disableStatus();  
         }
         
+    },
+
+    /*
+    * Se bloquea campo "Estado" al tener registro de Reunión como Realizada o No Realizasa
+    */
+
+    readOnlyStatus: function(){
+
+        if((this.model.get('status')=="Held" && !this.isCreate) || (this.model.get('status')=="Not Held" && !this.isCreate)){
+
+            $('select[name="status"]').parent().parent().addClass("field--readonly");
+            $('select[name="status"]').parent().attr("style","pointer-events:none");
+
+            //Se bloquea campo "Relacionado con"
+            $('.field.fast-click-highlighted>.field__controls--flex').parent().attr('style','pointer-events:none');
+            $('.field.fast-click-highlighted>.field__controls--flex').parent().addClass("field--readonly");
+
+        }
+
     },
 
 
