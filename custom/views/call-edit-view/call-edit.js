c0d0706895aed1f8c1825e001605010bc5a8a7b8
Salvador Lopez <salvador.lopez@tactos.com.mx>
2018-09-18 14:54:11 -0500

    Se bloquea campo Relacionado con en registro de Llamada

--- custom/views/call-edit-view/call-edit.js
+++ custom/views/call-edit-view/call-edit.js
@@ -203,6 +203,8 @@ const CallEditView = customization.extend(EditView, {
             this.model.on('sync', this.setIdParent(options), this);
         }
 
+        this.model.on('sync', this.readOnlyStatus,this);
+
     },
 
     _render: function()  {  
@@ -213,6 +215,25 @@ const CallEditView = customization.extend(EditView, {
             this.disableStatus();  
         }
         
+    },
+
+    /*
+    * Se bloquea campo "Estado" al tener registro de Reunión como Realizada o No Realizada
+    */
+
+    readOnlyStatus: function(){
+
+        if((this.model.get('status')=="Held" && !this.isCreate) || (this.model.get('status')=="Not Held" && !this.isCreate)){
+
+            $('select[name="status"]').parent().parent().addClass("field--readonly");
+            $('select[name="status"]').parent().attr("style","pointer-events:none");
+
+            //Se bloquea campo "Relacionado con"
+            $('.field.fast-click-highlighted>.field__controls--flex').parent().attr('style','pointer-events:none');
+            $('.field.fast-click-highlighted>.field__controls--flex').parent().addClass("field--readonly");
+
+        }
+
     },
 
 
