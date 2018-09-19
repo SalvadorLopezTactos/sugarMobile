aee5d9fcf8e5db2e2a6fa730bc48480f67875477
Jesus Carrillo <jesus.carrillo@tactos.com.mx>
2018-09-19 18:08:47 -0500

    devJCM|Incidencia 202|Concluida

--- custom/views/meetings-edit-view/meeting-edit.js
+++ custom/views/meetings-edit-view/meeting-edit.js
@@ -6,6 +6,8 @@ const EditView = require('%app.views.edit%/modules/meetings-calls-edit-view.js')
 const MeetingEditView = customization.extend(EditView, {
     initialize(options) {
         this._super(options);
+
+        this.model.on('sync', this.disableStatus2, this);
         
     },
 
@@ -14,20 +16,34 @@ const MeetingEditView = customization.extend(EditView, {
         this._super('_render');  
 
         if(this.isCreate){
-
-            this.disableStatus();  
+            this.disableStatus();
         }
-        
+
     },
 
 
     disableStatus: function(){
+            $('select[name="status"]').parent().parent().addClass("field--readonly");
+            $('select[name="status"]').parent().attr("style", "pointer-events:none");
+    },
 
-        $('select[name="status"]').parent().parent().addClass("field--readonly");
-        $('select[name="status"]').parent().attr("style","pointer-events:none");
+    disableStatus2: function(){
+        var arr=[];
+        $('input[data-type=time]').each(function(index, elem){
+            var a=$(elem).val().split(':');
+            var seconds = (a[0] * 60 * 60) + (a[1] * 60) ;
 
-    },  
+            arr.push(seconds);
+        });
+        var hour_end=Math.max.apply(null, arr);
+        var date_end=Date.parse(this.model.get('date_end'));
+        date_end+=hour_end;
 
+        if(date_end>Date.now() || app.user.attributes.full_name!=this.model.get('assigned_user_name')) {
+            $('select[name="status"]').parent().parent().addClass("field--readonly");
+            $('select[name="status"]').parent().attr("style", "pointer-events:none");
+        }
+    },
 
 });
 
